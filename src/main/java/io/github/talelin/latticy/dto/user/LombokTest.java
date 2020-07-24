536814eb8e5c1ad36d64678c5bc63e0ca3a10983
qiaogao.xiang <xqgdtx1@sina.com>
2020-07-24 17:55:30 +0800

    加入Lombok测试注解

---
+++ src/main/java/io/github/talelin/latticy/dto/user/LombokTest.java
@@ -0,0 +1,13 @@
+package io.github.talelin.latticy.dto.user;
+
+/**
+ * ********************************************************
+ *
+ * @Author:Joe
+ * @Date:Created in 10:34    2020/7/24
+ * @Description: lombok测试
+ * <p>
+ * ********************************************************
+ */
+public class LombokTest {
+}
